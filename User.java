3b6f6726d23d94a7ca0615b56e2cde60385d7bb7
Md Riasad <md.riasad@ntdac.naztech.local>
2019-04-01 16:02:38 +0600

    2nd Commit

---
+++ User.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+class User 
+{ 
+    String name, role; 
+    User(String a, String b) { 
+        name = a; 
+        role = b; 
+    } 
+    
+    String getRole() { return role; } 
+    String getName() { return name; } 
+    
+    public String toString() { 
+       return "User Name : " + name + ", Role :" + role; 
+    } 
+  
+    public static void main(String args[]) 
+    {       
+        List<User> users = new ArrayList<User>(); 
+        users.add(new User("John", "admin")); 
+        users.add(new User("Peter", "member"));
+        users.add(new User("Riasad", "admin"));
+        users.add(new User("Peter", "member"));
+        List admins = getAdmin(users, (User u) -> u.getRole().toUpperCase().equals("ADMIN")); 
+        System.out.println(admins); 
+    } 
+  
+    public static List<User> getAdmin(List<User> users,  
+                            Predicate<User> predicat) 
+    { 
+        List<User> result = new ArrayList<User>(); 
+        for (User user: users)         
+            if (predicat.test(user))             
+                result.add(user); 
+        return result; 
+    } 
+} 
